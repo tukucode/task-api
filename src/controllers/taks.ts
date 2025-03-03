0ba26be6bd6b9199d55b95e5c554a4ccc7b482e0
tomimandalap <tommymandalaputra@gmail.com>
2025-03-03 14:47:11 +0700

    feat: added jsDoc in task controller

--- src/controllers/taks.ts
+++ src/controllers/taks.ts
@@ -1,8 +1,7 @@
 import { z } from 'zod';
 import { Task } from '../types/task';
-import { tasks } from '../db/data'
-import { sendResponse, validateRequestBody } from '../lib/utils'
-
+import { tasks } from '../db/data';
+import { sendResponse, validateRequestBody } from '../lib/utils';
 import type { Request, Response } from 'express';
 
 const taskSchema = z.object({
@@ -11,60 +10,91 @@ const taskSchema = z.object({
     completed: z.boolean()
 });
 
-export const newTask = [validateRequestBody(taskSchema), (req: Request, res: Response) => {
-    const task: Task = {
-        id: tasks.length + 1,
-        title: req.body.title,
-        description: req.body.description,
-        completed: false,
-    };
+/**
+ * Middleware untuk membuat task baru.
+ * @param {taskSchema} schema - Objek schema yang merujuk untuk validation req body sebagai parameter pada middleware.
+ * @param {Request} req - Objek request dari Express.
+ * @param {Response} res - Objek response dari Express.
+ * @returns {Response} - Response dengan task yang baru dibuat.
+ */
+export const newTask = [
+    validateRequestBody(taskSchema), 
+    (req: Request, res: Response) => {
+        const task: Task = {
+            id: tasks.length + 1,
+            title: req.body.title,
+            description: req.body.description,
+            completed: false,
+        };
 
-    tasks.push(task);
-    return sendResponse<Task>(res, 201, "Success create taks", task)
-}]
+        tasks.push(task);
+        return sendResponse<Task>(res, 201, "Success create task", task);
+    }
+];
 
+/**
+ * Mengembalikan daftar semua task yang ada.
+ * @param {Request} req - Objek request dari Express.
+ * @param {Response} res - Objek response dari Express.
+ */
 export const listTask = (req: Request, res: Response) => {
-    sendResponse<Task[]>(res, 200, "Success", tasks)
-}
+    sendResponse<Task[]>(res, 200, "Success", tasks);
+};
 
+/**
+ * Mengembalikan detail dari task tertentu berdasarkan ID.
+ * @param {Request} req - Objek request dari Express.
+ * @param {Response} res - Objek response dari Express.
+ */
 export const detailTask = (req: Request, res: Response) => {
     const task = tasks.find((data) => data.id === parseInt(req.params.id));
 
     if (!task) {
-        return sendResponse(res, 400, "Invalid taks ID", null)
+        return sendResponse(res, 400, "Invalid task ID", null);
     }
 
-    sendResponse<Task>(res, 200, "Success", task)
-}
+    sendResponse<Task>(res, 200, "Success", task);
+};
 
 const completedSchema = z.object({
     completed: z.boolean()
 });
 
-export const completedTask =  [validateRequestBody(completedSchema), (req: Request, res: Response) => {
-    // Find the index of the task
-    const index = tasks.findIndex((task) => task.id === parseInt(req.params.id));
+/**
+ * Middleware untuk memperbarui status completed dari task.
+ * @param {completedSchema} schema - Objek schema yang merujuk untuk validation req body sebagai parameter pada middleware.
+ * @param {Request} req - Objek request dari Express.
+ * @param {Response} res - Objek response dari Express.
+ * @returns {Response} - Response setelah memperbarui status task.
+ */
+export const completedTask = [
+    validateRequestBody(completedSchema), 
+    (req: Request, res: Response) => {
+        const index = tasks.findIndex((task) => task.id === parseInt(req.params.id));
 
-    if (index === -1) {
-        return sendResponse(res, 400, "Invalid taks ID", null)
+        if (index === -1) {
+            return sendResponse(res, 400, "Invalid task ID", null);
+        }
+
+        tasks[index] = { ...tasks[index], completed: req.body.completed };
+
+        sendResponse(res, 200, "Update task completed", tasks[index]);
     }
+];
 
-    // Update task by index
-    tasks[index] = { ...tasks[index], completed: req.body.completed};
-
-    sendResponse(res, 200, "Update taks completed", tasks[index])
-}]
-
+/**
+ * Menghapus task berdasarkan ID.
+ * @param {Request} req - Objek request dari Express.
+ * @param {Response} res - Objek response dari Express.
+ */
 export const deleteTask = (req: Request, res: Response) => {
-    // Find the index of the task
     const index = tasks.findIndex((task) => task.id === parseInt(req.params.id));
 
     if (index === -1) {
-        return sendResponse(res, 400, "Invalid taks ID", null)
+        return sendResponse(res, 400, "Invalid task ID", null);
     }
 
-    // Splice task by index
     tasks.splice(index, 1);
 
-    sendResponse(res, 200, "Delete taks completed", null)
-}
+    sendResponse(res, 200, "Delete task completed", null);
+};
